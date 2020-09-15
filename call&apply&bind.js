8e9d0988623f53444134c0795968de750e562e5b
longzhouLab <longzhou@heytea.com>
2020-09-15 23:33:08 +0800

    update: call apply bind 实现

--- call&apply&bind.js
+++ call&apply&bind.js
@@ -1,17 +1,45 @@
-Function.prototype.newCall = function () {
-
+Function.prototype.newCall = function (...arg) {
+  let target = arg[0]
+  if (!target) {
+    if (typeof window !== 'undefined') {
+      target = window
+    }
+    if (typeof global !== 'undefined') {
+      target = global
+    }
+  }
+  target.bindThis = this
+  target.bindThis(...arg.slice(1))
+  delete target.bindThis
 }
-
-
-
 
 Function.prototype.newApply = function () {
-  
+  let target = arg[0]
+  if (!target) {
+    if (typeof window !== 'undefined') {
+      target = window
+    }
+    if (typeof global !== 'undefined') {
+      target = global
+    }
+  }
+  target.bindThis = this
+  target.bindThis(arg.slice(1))
+  delete target.bindThis
 }
 
-
-
-
-Function.prototype.newBind = function () {
-
+Function.prototype.newBind = function (...arg) {
+  let _this = this
+  let target = arg[0]
+  if (!target) {
+    if (typeof window !== 'undefined') {
+      target = window
+    }
+    if (typeof global !== 'undefined') {
+      target = global
+    }
+  }
+  return function (...newArg) {
+    return _this.apply(target, arg.slice(1).concat(newArg))
+  }
 }
